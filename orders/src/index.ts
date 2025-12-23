4f624052fce8bf8f636bd34fa18dd772f29b55da
Murali981 <muraliju981@gmail.com>
2025-12-23 07:28:38 +0530

    Implemented the events publisher and listener for ticket created events

---
+++ orders/src/index.ts
@@ -0,0 +1,57 @@
+import mongoose from "mongoose";
+import { app } from "./app";
+import { natswrapper } from "./nats-wrapper";
+import { randomBytes } from "crypto";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+  try {
+    await natswrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL
+    );
+    natswrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit(); // Exit the process when NATS connection is closed
+    });
+
+    process.on("SIGINT", () => {
+      natswrapper.client.close();
+    }); // Interrupt signal (Ctrl+C), This is watching for the interrupt signal at anytime when the ts-node-dev tries to restart
+    // our program at anytime when you hit ctrl + C at our terminal. So we are going to intercept this request through this
+    // SIGINT event and then we are going to close the NATS connection before exiting the program.
+
+    process.on("SIGTERM", () => {
+      natswrapper.client.close();
+    }); // Termination signal , This is watching for the termination signal at anytime when the container in which our service is running
+    // is being shutdown. So we are going to intercept this request through this SIGTERM event and then we are going to close
+    // the NATS connection before exiting the program.
+    await mongoose.connect(process.env.MONGO_URI); // Here we are connecting to the mongoDB instance which is running on another pod. So we have
+    // to go through the clusterIP service to connect to the mongoDB instance which is running on that pod.
+    // auth in /auth is name of the database which is auth that we want to create.
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!");
+  });
+};
+
+start();
