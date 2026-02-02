46877f8f423235b7a9a9bbe958d814d4b8dfb609
Murali981 <muraliju981@gmail.com>
2026-02-02 15:21:12 +0530

    Commiting latest changes

---
+++ expiration/src/index.ts
@@ -0,0 +1,40 @@
+import { natswrapper } from "./nats-wrapper";
+
+const start = async () => {
+  if (!process.env.NATS_CLIENT_ID) {
+    throw new Error("NATS_CLIENT_ID must be defined");
+  }
+  if (!process.env.NATS_URL) {
+    throw new Error("NATS_URL must be defined");
+  }
+  if (!process.env.NATS_CLUSTER_ID) {
+    throw new Error("NATS_CLUSTER_ID must be defined");
+  }
+  try {
+    await natswrapper.connect(
+      process.env.NATS_CLUSTER_ID,
+      process.env.NATS_CLIENT_ID,
+      process.env.NATS_URL,
+    );
+    natswrapper.client.on("close", () => {
+      console.log("NATS connection closed");
+      process.exit(); // Exit the process when NATS connection is closed
+    });
+
+    process.on("SIGINT", () => {
+      natswrapper.client.close();
+    }); // Interrupt signal (Ctrl+C), This is watching for the interrupt signal at anytime when the ts-node-dev tries to restart
+    // our program at anytime when you hit ctrl + C at our terminal. So we are going to intercept this request through this
+    // SIGINT event and then we are going to close the NATS connection before exiting the program.
+
+    process.on("SIGTERM", () => {
+      natswrapper.client.close();
+    }); // Termination signal , This is watching for the termination signal at anytime when the container in which our service is running
+    // is being shutdown. So we are going to intercept this request through this SIGTERM event and then we are going to close
+    // the NATS connection before exiting the program.
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+start();
