40468b305cc8f643088a10945dd7ef23adad38ff
Murali981 <muraliju981@gmail.com>
2026-02-22 18:18:29 +0530

    Updated the startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Starting up the server....");
+  console.log("Starting up the server........");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
