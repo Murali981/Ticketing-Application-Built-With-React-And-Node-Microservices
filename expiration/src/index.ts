031bdc62a3590daf1989cb578e12da3d3003cf5f
Murali981 <muraliju981@gmail.com>
2026-02-23 12:53:57 +0530

    new startup messages

--- expiration/src/index.ts
+++ expiration/src/index.ts
@@ -2,6 +2,7 @@ import { natswrapper } from "./nats-wrapper";
 import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 const start = async () => {
+  console.log("Starting the server...");
   if (!process.env.NATS_CLIENT_ID) {
     throw new Error("NATS_CLIENT_ID must be defined");
   }
